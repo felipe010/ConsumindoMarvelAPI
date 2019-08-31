5087871a4f71d3dda6a58e0c6eee7d60004ad48a
Felipe <fox010@gmail.com>
2019-08-31 13:46:58 -0300

    Construção da função que desenha o personagem "Depois de observar meu resultado em JSON, encontrei oq queria o atributo 'data' com um array com os 20 primeiros personagens que a api me retorna." observe no código como fiz para chamar a função que processa cada item desse array e usa uma função para inserir tags HTML no personagemchanges.

--- js/script.js
+++ js/script.js
@@ -14,8 +14,22 @@ const privateKey = '90d1bb3bf2602eb4dc2376a9f6526d5112d68f6f',
     fetch(URL)
         .then(response => response.json()) //Aqui informo que quero o resultado em JSON
         .then(response => {
-            console.log(response); /* Aqui já consigo ver o resultado em JSON, e ver o response em JSON no qual tem um atributo  'data' que contem um 'results' e nele contem o resultado que queremos. */
+            console.log(response); /* Aqui já consigo ver o resultado em JSON, e ver o response em JSON no qual tem um atributo  'data' que contem um 'results' e nele contem um array com o resultado que queremos. */
+            response.data.results.array.forEach(element => {
+                drawHero(element);
+            });
         });
+}
+
+const drawHero = element => {
+    const image = `${element.thumbnail.path}/portrait_uncanny.${element.thumbnail.extension}` //Entenda que thumbnail' é um objeto  que contem a propriedade 'path' e 'extension' e claro o thumbnail faz parte do cada item do meu array resultado. 
+    const hero = `
+        <div>
+            <h3>${element.name}</h3>
+            <img src"${image}">
+            <p>${element.description}</p>
+        </div>
+    `;
 }
 
 getConnection();

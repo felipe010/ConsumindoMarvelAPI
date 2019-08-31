b7bbbe293e9eb7716dfabf9f996cb92e6628dfb0
Felipe <fox010@gmail.com>
2019-08-31 14:43:46 -0300

    Pequenas correções, e adicionado linha que insere Heroi na página HTML

--- js/script.js
+++ js/script.js
@@ -14,8 +14,10 @@ const privateKey = '90d1bb3bf2602eb4dc2376a9f6526d5112d68f6f',
     fetch(URL)
         .then(response => response.json()) //Aqui informo que quero o resultado em JSON
         .then(response => {
-            console.log(response); /* Aqui já consigo ver o resultado em JSON, e ver o response em JSON no qual tem um atributo  'data' que contem um 'results' e nele contem um array com o resultado que queremos. */
-            response.data.results.array.forEach(element => {
+            //console.log(response); /* Aqui já consigo ver o resultado em JSON, e ver o response em JSON no qual tem um atributo  'data' que contem um 'results' e nele contem um array com o resultado que queremos. */
+            //console.log(response.data.results);
+                        
+            response.data.results.forEach(element => {
                 drawHero(element);
             });
         });
@@ -23,13 +25,17 @@ const privateKey = '90d1bb3bf2602eb4dc2376a9f6526d5112d68f6f',
 
 const drawHero = element => {
     const image = `${element.thumbnail.path}/portrait_uncanny.${element.thumbnail.extension}` //Entenda que thumbnail' é um objeto  que contem a propriedade 'path' e 'extension' e claro o thumbnail faz parte do cada item do meu array resultado. 
+    //Dentro da template string tem uma string '/portrait_uncanny', ela está ali para retornar as imagens em tamanho menores, não deixando o carregamento lento.
     const hero = `
         <div>
             <h3>${element.name}</h3>
-            <img src"${image}">
+            <img src="${image}">
             <p>${element.description}</p>
         </div>
     `;
+
+    content.insertAdjacentHTML('beforeEnd', hero); //Insere cada DIV (de heroi) como filha na DIV pai chamada 'content'
+
 }
 
 getConnection();
